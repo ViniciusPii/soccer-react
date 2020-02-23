27a5dca692739b434e03e57582f5f460605d91c2
Pii <pii@Piis-iMac.local>
2020-02-23 01:56:34 -0300

    Lista de times

--- src/pages/list-team/ListTeam.js
+++ src/pages/list-team/ListTeam.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Table, Button } from 'reactstrap';
+import axios from 'axios';
+
+import { orderBy } from 'lodash'
 
 const ListTeam = () => {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://brazilian-league.herokuapp.com/teams`)
+      .then(res => {
+        setData(orderBy(res.data, ['name']))
+      })
+  }, [])
+
+  const renderRow = team => {
+    return (
+      <tr key={team.id}>
+        <th scope="row"><img src={team.urlLogo} style={{height: 70}}></img></th>
+        <td className="align-middle" style={{textTransform: 'uppercase'}}>{team.name}</td>
+        <th className="text-right align-middle"><Button color="info">Escalação</Button>{' '}</th>
+      </tr>
+    )
+  }
+
   return (
-    <h1>Olá list</h1>
-  )
-} 
+    <div className="container mt-5 col-md-6">
+      <Table>
+        <thead>
+          <tr>
+            <th>Escudo</th>
+            <th>Nome</th>
+            <th className="text-right">Escalação</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(renderRow)}
+        </tbody>
+      </Table>
+    </div>
+  );
+}
 
 export default ListTeam;
